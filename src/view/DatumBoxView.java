44bed1973fcc71e6c60666849775f8a9a1a1660e
Brian <bgreeneaka@gmail.com>
2015-03-10 18:25:11 +0000

    Sort of working

--- src/view/DatumBoxView.java
+++ src/view/DatumBoxView.java
@@ -156,4 +156,10 @@ public class DatumBoxView extends JScrollPane implements Observer {
 		return this.viewRef;
 	}
 
+	@Override
+	public void repaintParent() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/view/RapidMinerView.java
+++ src/view/RapidMinerView.java
@@ -146,4 +146,10 @@ public class RapidMinerView extends JScrollPane implements Observer{
 		// TODO Auto-generated method stub
 		return this.viewRef;
 	}
+
+	@Override
+	public void repaintParent() {
+		// TODO Auto-generated method stub
+		
+	}
 }
